35b787bf575a7201bfc860c28b87f3537834be47
tokotun <saar.2012@yandex.ru>
2015-05-23 18:54:35 +0300

    lesson 11. change sorting. Verbal key.

--- lesson_11.js
+++ lesson_11.js
@@ -5,4 +5,12 @@ function getPopulation (table, i) {
 function getName (table, i) {
     return table.rows[i].cells[2].textContent;
 }
+function sortFunction(a, b) {
+  if (a.population > b.population)
+     return -1;
+  if (a.population < b.population)
+     return 1;
+  return 0;
+}
+
 
